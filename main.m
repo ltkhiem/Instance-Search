746d41633d98b76faa4444239a22e1df74fce70a
nvtiep <nguyenvinhtiep@gmail.com>
2014-02-04 01:18:57 +0700

    Report + edit code

--- main.m
+++ main.m
@@ -105,36 +105,54 @@ end
 
 % create an inverted file for the images
 fprintf('Creating and searching an inverted file\n');
-if exist('inverted_file.mat', 'file')
-    load('inverted_file.mat');
-else
-    if_weight = 'tfidf';
-    if_norm = 'l2';
-    if_dist = 'l2';
-    inv_file = ccvInvFileInsert([], words, num_words);
-    ccvInvFileCompStats(inv_file, if_weight, if_norm);
-    %save('inverted_file.mat', 'if_weight', 'if_norm', 'if_dist', 'inv_file');
-end
+if_weight = 'tfidf';
+if_norm = 'l1';
+if_dist = 'l1';
+inv_file = ccvInvFileInsert([], words, num_words);
+ccvInvFileCompStats(inv_file, if_weight, if_norm);
+%save('inverted_file.mat', 'if_weight', 'if_norm', 'if_dist', 'inv_file');
+
 %% Query images
 verbose=0;
 q_files = dir(fullfile('C:\oxford-groundtruth', '*query.txt'));
 %oxc1_all_souls_000013 136.5 34.1 648.5 955.7
-ntop = 200;
+ntop = 0;
 % load query image
-for k=1:length(q_files)
+for k=34:length(q_files)
     k
     fid = fopen(strcat('C:\oxford-groundtruth\', q_files(k).name), 'r');
     str = fgetl(fid);
     [image_name, remain] = strtok(str, ' ');
     fclose(fid);
+    numbers = str2num(remain);
+    x1 = numbers(1);
+    y1 = numbers(2);
+    x2 = numbers(3);
+    y2 = numbers(4);
     file = strcat('C:\oxford-images\', image_name(6:end), '.jpg');
     I = im2single(rgb2gray(imread(file)));
+    %imshow(I); hold on;
+    %plot([x1 x2], [y1 y1], 'g');
+    %plot([x2 x2], [y1 y2], 'g');
+    %plot([x2 x1], [y2 y2], 'g');
+    %plot([x1 x1], [y2 y1], 'g');
+    %hold off;
     % compute rootSIFT features
     [frame, sift] = vl_covdet(I, 'method', 'Hessian', 'estimateAffineShape', true);
-    root_sift = zeros(size(sift));
+    idx = zeros(1, size(frame, 2));
+    for i=1:size(frame,2)
+        if frame(1,i) <= x2 && frame(1,i) >= x1 && frame(2,i) <= y2 && frame(2,i) >= y1
+            idx(i) = 1;
+        end
+    end
+    root_sift = zeros(128, sum(idx));
     nfeat = size(sift, 2);
+    c=1;
     for i=1:nfeat
-        root_sift(:,i) = sqrt(sift(:,i) / sum(sift(:,i)));
+        if idx(i)==1
+            root_sift(:,c) = sqrt(sift(:,i) / sum(sift(:,i)));
+            c=c+1;
+        end
     end
     % Test on an image
     q_words = cell(1,1);
@@ -147,31 +165,35 @@ for k=1:length(q_files)
         title(image_name(6:end));
     end
     fid = fopen('c:\oxford-groundtruth\rank_list.txt', 'w');
-    for i=1:ntop
-        % Show only 10 highest score image
+    for i=1:size(ids{1},2)
+        % Show only 10 highest score images
         if verbose==1 && i<=10
             subplot(3, 5, 5+i); 
-            imshow(imread(strcat('C:\oxford-images\', files(ids(i)).name)));
-            title(files(ids(i)).name);
+            imshow(imread(strcat('C:\oxford-images\', files(ids{1}(i)).name)));
+            title(files(ids{1}(i)).name);
         end
-        fprintf(fid, '%s\n', files(ids(i)).name(1:end-4));
+        fprintf(fid, '%s\n', files(ids{1}(i)).name(1:end-4));
     end
     fclose(fid);
     script = ['c:\oxford-groundtruth\Test.exe c:\oxford-groundtruth\', ...
         q_files(k).name(1:end-10), ...
         ' c:\oxford-groundtruth\rank_list.txt',...
-        ' >result\', q_files(k).name(1:end-10), '_result.txt'];
+        ' >result\', image_name(6:end), '_result.txt']; %q_files(k).name(1:end-10)
     system(script);
+    if verbose==1
+        pause;
+    end
 end
 
-q_files = dir(fullfile('.\result\', '*.txt'));
+r_files = dir(fullfile('.\result\', '*.txt'));
 acc = [];
-for i=1:length(q_files)
-    file = ['.\result\' q_files(i).name];
+for i=1:length(r_files)
+    file = ['.\result\' r_files(i).name];
     fid = fopen(file, 'r');
     acc = [acc fscanf(fid, '%f')];
     fclose(fid);
 end
 mean(acc)
+
 % clear inv file
 ccvInvFileClean(inv_file);
